c0a33f3a6e6b4bff2dfedea315aa07db16123a67
junlong.huang <none@gmail.com>
2026-04-03 00:30:32 +0800

    cleanup startup permissions and app manifest

--- module_core/src/main/java/com/hjl/core/MainActivity.kt
+++ module_core/src/main/java/com/hjl/core/MainActivity.kt
@@ -1,6 +1,5 @@
 package com.hjl.core
 
-
 import android.Manifest
 import android.os.Bundle
 import androidx.fragment.app.Fragment
@@ -13,7 +12,6 @@ import com.hjl.core.ui.minus.MinusFragment
 import com.hjl.jetpacklib.mvvm.view.BaseActivity
 import com.permissionx.guolindev.PermissionX
 
-
 class MainActivity : BaseActivity<CoreActivityMainBinding>() {
 
     override fun getLayoutId(): Int {
@@ -21,47 +19,44 @@ class MainActivity : BaseActivity<CoreActivityMainBinding>() {
     }
 
     override fun initData() {
-
     }
 
     override fun initView() {
-        val fragments = arrayListOf<Fragment>(MinusFragment(),MainFragment())
+        val fragments = arrayListOf<Fragment>(MinusFragment(), MainFragment())
         val adapter = LazyFragmentPagerAdapter(supportFragmentManager, fragments)
         binding.activityMainVp.adapter = adapter
 //        binding.activityMainVp.offscreenPageLimit = fragments.size
-        binding.activityMainVp.setCurrentItem(1,false)
-
+        binding.activityMainVp.setCurrentItem(1, false)
     }
 
     override fun initLoad() {
         PermissionX.init(this).permissions(
-            Manifest.permission.ACCESS_LOCATION_EXTRA_COMMANDS,
-            Manifest.permission.CHANGE_NETWORK_STATE,
-            Manifest.permission.CHANGE_WIFI_STATE,
-            Manifest.permission.WRITE_EXTERNAL_STORAGE,
             Manifest.permission.ACCESS_COARSE_LOCATION,
-            Manifest.permission.ACCESS_FINE_LOCATION).onExplainRequestReason { deniedList  ->
-            showRequestReasonDialog(
-                deniedList,
-                BaseApplication.getApplication().getString(R.string.ths_prmssn_s_t_nsr_),
-                BaseApplication.getApplication().getString(R.string.i_understand_now),
-                BaseApplication.getApplication().getString(R.string.cancel)
-            )
-        }.onForwardToSettings { deniedList ->
-            showForwardToSettingsDialog(
-                deniedList,
-                BaseApplication.getApplication().getString(R.string.y_nd_t_mnlly_nbl_pr),
-                BaseApplication.getApplication().getString(R.string.i_understand_now),
-                BaseApplication.getApplication().getString(R.string.cancel)
-            )
-        }.request { allGranted, grantedList, deniedList ->
-            if (allGranted) {
-                LogUtils.e(TAG, "所有申请的权限都已通过:$grantedList")
-            } else {
-                LogUtils.e(TAG, "权限被拒绝:$deniedList")
+            Manifest.permission.ACCESS_FINE_LOCATION
+        )
+            .onExplainRequestReason { deniedList ->
+                showRequestReasonDialog(
+                    deniedList,
+                    BaseApplication.getApplication().getString(R.string.ths_prmssn_s_t_nsr_),
+                    BaseApplication.getApplication().getString(R.string.i_understand_now),
+                    BaseApplication.getApplication().getString(R.string.cancel)
+                )
             }
-
-        }
+            .onForwardToSettings { deniedList ->
+                showForwardToSettingsDialog(
+                    deniedList,
+                    BaseApplication.getApplication().getString(R.string.y_nd_t_mnlly_nbl_pr),
+                    BaseApplication.getApplication().getString(R.string.i_understand_now),
+                    BaseApplication.getApplication().getString(R.string.cancel)
+                )
+            }
+            .request { allGranted, grantedList, deniedList ->
+                if (allGranted) {
+                    LogUtils.i(TAG, "Granted permissions: $grantedList")
+                } else {
+                    LogUtils.e(TAG, "Denied permissions: $deniedList")
+                }
+            }
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
